80a92c0a746462021e6e74acc4cfd03cbec133ba
sandevD <sandev.net@gmail.com>
2025-03-15 15:25:12 +0530

    ✨ Add fillable attributes and classroom relationship to Course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'max_student_limit',
+        'description',
+        'day',
+        'start_time',
+        'end_time',
+        'starting_date',
+        'ending_date',
+    ];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
 }
