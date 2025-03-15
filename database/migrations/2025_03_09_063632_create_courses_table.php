3bb3ad69123080214c3b18cef358cefd43059c27
sandevD <sandev.net@gmail.com>
2025-03-15 15:25:21 +0530

    ✨ Update courses table migration to include classroom relationship, unique name, max student limit, and schedule fields

--- database/migrations/2025_03_09_063632_create_courses_table.php
+++ database/migrations/2025_03_09_063632_create_courses_table.php
@@ -13,8 +13,15 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
+            $table->foreignId('classroom_id')->nullable()->references('id')->on('classrooms')->cascadeOnDelete();
+            $table->string('name')->unique();
+            $table->integer('max_student_limit')->default(100);
+            $table->longText('description')->nullable();
+            $table->string('day');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->timestamp('starting_date');
+            $table->timestamp('ending_date');
             $table->timestamps();
         });
     }
