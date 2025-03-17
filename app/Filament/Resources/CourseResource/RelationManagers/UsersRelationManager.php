62247adf62db22478ad4ea0210eb48559703b5ad
sandevD <sandev.net@gmail.com>
2025-03-18 00:24:12 +0530

    🎨 User Relation Manager

---
+++ app/Filament/Resources/CourseResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources\CourseResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UsersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'users';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('admission_no')
+                    ->searchable()
+                    ->label('Admission No'),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->label('Full Name'),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->label('Email'),
+                TextColumn::make('mobile_no')
+                    ->searchable()
+                    ->label('Mobile'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                //Tables\Actions\CreateAction::make(),
+                Tables\Actions\AttachAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                //Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    //Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DetachBulkAction::make(),
+                ]),
+            ]);
+    }
+}
