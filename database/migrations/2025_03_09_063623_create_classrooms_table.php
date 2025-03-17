478aa09c7f98c2b0db22bf9c9558f379661c84af
sandevD <sandev.net@gmail.com>
2025-03-18 00:26:17 +0530

    🗃️  classrooms table

--- database/migrations/2025_03_09_063623_create_classrooms_table.php
+++ database/migrations/2025_03_09_063623_create_classrooms_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('classrooms', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->foreignId('building_id')->nullable()->references('id')->on('buildings')->cascadeOnDelete();
             $table->timestamps();
         });
     }
