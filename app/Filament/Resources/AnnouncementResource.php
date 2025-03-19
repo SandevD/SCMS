4a25f6b4964bef51e8bfd26d801eac169cc09bc2
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-03-19 15:50:23 +0530

    🎨  Update navigation sort order for Announcement resource

--- app/Filament/Resources/AnnouncementResource.php
+++ app/Filament/Resources/AnnouncementResource.php
@@ -23,7 +23,7 @@ class AnnouncementResource extends Resource
 
     protected static ?string $navigationGroup = 'Announcements';
 
-    protected static ?int $navigationSort = 3;
+    protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
     {
