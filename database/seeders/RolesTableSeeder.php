64cf2a6aa717de83dd178d4902b40f89cfbf78f3
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-03-19 15:52:17 +0530

    ✨ Update RolesTableSeeder to add Lecturer and Student roles

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -14,12 +14,12 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('roles')->delete();
-
+        
         \DB::table('roles')->insert(array (
-            0 =>
+            0 => 
             array (
                 'id' => 1,
                 'name' => 'Super Admin',
@@ -27,8 +27,24 @@ class RolesTableSeeder extends Seeder
                 'created_at' => '2024-10-15 05:57:00',
                 'updated_at' => '2024-10-15 05:57:00',
             ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Lecturer',
+                'guard_name' => 'web',
+                'created_at' => '2025-03-19 10:03:58',
+                'updated_at' => '2025-03-19 10:03:58',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Student',
+                'guard_name' => 'web',
+                'created_at' => '2025-03-19 10:04:09',
+                'updated_at' => '2025-03-19 10:04:09',
+            ),
         ));
-
-
+        
+        
     }
 }
