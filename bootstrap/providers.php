ff5e7773941859eb81d553bb4462403165336f4e
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-03-19 15:51:43 +0530

    ✨ Add StudentPanelProvider to bootstrap providers for student management

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
+    App\Providers\Filament\StudentPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
