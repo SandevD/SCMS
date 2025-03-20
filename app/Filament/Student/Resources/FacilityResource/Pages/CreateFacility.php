398b3d6e974a401c9990ab388968444751f86ec5
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-03-20 18:53:20 +0530

    ✨ Add CreateFacility page for managing facility records

---
+++ app/Filament/Student/Resources/FacilityResource/Pages/CreateFacility.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Student\Resources\FacilityResource\Pages;
+
+use App\Filament\Student\Resources\FacilityResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFacility extends CreateRecord
+{
+    protected static string $resource = FacilityResource::class;
+}
