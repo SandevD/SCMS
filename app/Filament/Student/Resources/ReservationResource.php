fc5b3d11681103df4ffcab958b33f2be8f5339de
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-03-20 18:53:05 +0530

    ✨ Enhance ReservationResource with detailed reservation information and status representation

--- app/Filament/Student/Resources/ReservationResource.php
+++ app/Filament/Student/Resources/ReservationResource.php
@@ -2,13 +2,20 @@
 
 namespace App\Filament\Student\Resources;
 
+use App\Enum\ReservationStatus;
 use App\Filament\Student\Resources\ReservationResource\Pages;
 use App\Filament\Student\Resources\ReservationResource\RelationManagers;
+use App\Models\Event;
+use App\Models\Facility;
 use App\Models\Reservation;
+use App\Models\Workshop;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -31,15 +38,58 @@ class ReservationResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('model')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Type'),
+                TextColumn::make('model_id')
+                    ->searchable()
+                    ->sortable()
+                    ->formatStateUsing(function ($record) {
+                        if ($record->model == 'EVENT') {
+                            $event = Event::find($record->model_id);
+                            return $event->name;
+                        }
+                        if ($record->model == 'WORKSHOP') {
+                            $workshop = Workshop::find($record->model_id);
+                            return $workshop->name;
+                        }
+                        if ($record->model == 'FACILITY') {
+                            $facility = Facility::find($record->model_id);
+                            return $facility->name;
+                        }
+                        return $record->model_id;
+                    })
+                    ->label('Type'),
+                TextColumn::make('start')
+                    ->searchable()
+                    ->sortable()
+                    ->dateTime()
+                    ->label('Start'),
+                TextColumn::make('end')
+                    ->searchable()
+                    ->sortable()
+                    ->dateTime()
+                    ->label('End'),
+                BadgeColumn::make('status')
+                    ->formatStateUsing(fn(int $state): string => match ($state) {
+                        ReservationStatus::COMPLETE->value => 'Complete',
+                        ReservationStatus::PENDING->value => 'Pending',
+                        ReservationStatus::REJECTED->value => 'Rejected',
+                        ReservationStatus::CANCELLED->value => 'Cancelled',
+                    })
+                    ->color(fn(int $state): string => match ($state) {
+                        ReservationStatus::COMPLETE->value => 'success', // Green
+                        ReservationStatus::PENDING->value => 'warning', // Yellow
+                        ReservationStatus::REJECTED->value => 'danger', // Red
+                        ReservationStatus::CANCELLED->value => 'gray', // Gray
+                    })
             ])
             ->filters([
                 //
             ])
-            ->actions([
-            ])
-            ->bulkActions([
-            ]);
+            ->actions([])
+            ->bulkActions([]);
     }
 
     public static function getRelations(): array
