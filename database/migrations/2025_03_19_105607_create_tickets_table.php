65311fd744bc66c992048c5a1831db5bf600b5f6
Sandev Dullewa <111951766+SandevD@users.noreply.github.com>
2025-03-20 18:53:43 +0530

    ✨ Add user_id, reason, and description fields to tickets table for improved tracking

--- database/migrations/2025_03_19_105607_create_tickets_table.php
+++ database/migrations/2025_03_19_105607_create_tickets_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->references('id')->on('users')->cascadeOnDelete();
+            $table->string('reason');
+            $table->string('description');
             $table->timestamps();
         });
     }
